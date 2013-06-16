f8321872a473a007e247ccb107c95a91882adf67
Howard Chung <Howard@Howard-Chungs-MacBook-Pro.local>
2013-06-16 16:43:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialtracker2::Application.config.secret_token = '63c83bd9305d1fa3d192eabebc5e0a3da4ac3b8d600a2bf381be6cad98778a3a7fe901ce14109e5b0b28ca868d79dab9476451580af3a7178cb471b540c421e0'
